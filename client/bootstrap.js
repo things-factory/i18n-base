b6444ceb8a420422bac218caafb659e5d6811e39
Hearty Oh <heartyoh@hatiolab.com>
2019-09-18 17:25:24 +0800

    v0.2.23
    i18n 변경시 context를 update하도록 bootstrap 로직 추가함.

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,1 +1,22 @@
-export default function bootstrap() {}
+import { store, UPDATE_CONTEXT } from '@things-factory/shell'
+export { i18next } from './i18next-config'
+
+function updateContext() {
+  store.dispatch({
+    type: UPDATE_CONTEXT
+  })
+}
+
+export default function bootstrap() {
+  i18next.on('initialized', options => {
+    updateContext()
+  })
+
+  i18next.on('languageChanged', () => {
+    updateContext()
+  })
+
+  i18next.store.on('added', (lng, ns) => {
+    updateContext()
+  })
+}
