f9d7652a4b70d43ef67c2e1792260dd6a5decc74
Hearty Oh <heartyoh@hatiolab.com>
2019-09-20 14:19:28 +0800

    v0.3.0

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,4 +1,4 @@
-import { store, UPDATE_CONTEXT } from '@things-factory/shell'
+import { store, UPDATE_CONTEXT, PageView } from '@things-factory/shell'
 import { i18next } from './i18next-config'
 
 function updateContext() {
@@ -8,15 +8,18 @@ function updateContext() {
 }
 
 export default function bootstrap() {
-  i18next.on('initialized', options => {
-    updateContext()
-  })
+  /*
+   * language와 관련된 변화는 항상 UPDATE_CONTEXT를 발생시켜서 다국어와 관련한 context기반의 변화를 유도한다.
+   */
+  i18next.on('initialized', updateContext)
+  i18next.on('languageChanged', updateContext)
+  i18next.store.on('added', updateContext)
 
-  i18next.on('languageChanged', () => {
-    updateContext()
-  })
-
-  i18next.store.on('added', (lng, ns) => {
-    updateContext()
-  })
+  /*
+   * PageView의 lifecycle에 languageUpdate를 추가한다.
+   */
+  PageView.prototype.languageUpdated = function(i18next) {
+    /* language가 바뀌는 전체적인 변화는 일반적으로 page의 재설정이 요구된다. */
+    this.pageInit()
+  }
 }
--- client/mixins/localize-mixin.js
+++ client/mixins/localize-mixin.js
@@ -4,26 +4,30 @@
 
 export const localize = i18next => baseElement =>
   class extends baseElement {
-    languageUpdated(i18next) {}
-
     connectedCallback() {
-      i18next.on('initialized', options => {
-        this.languageUpdated(i18next)
+      var callback = () => {
+        this.languageUpdated && this.languageUpdated(i18next)
         this.requestUpdate()
-      })
+      }
 
-      i18next.on('languageChanged', () => {
-        this.languageUpdated(i18next)
-        this.requestUpdate()
-      })
+      i18next.on('initialized', callback)
+      i18next.on('languageChanged', callback)
+      i18next.store.on('added', callback)
 
-      i18next.store.on('added', (lng, ns) => {
-        this.languageUpdated(i18next)
-        this.requestUpdate()
-      })
+      this.__i18next_callback__ = callback
 
       if (super.connectedCallback) {
         super.connectedCallback()
       }
+    }
+
+    disconnectedCallback() {
+      if (super.connectedCallback) {
+        super.connectedCallback()
+      }
+
+      i18next.store.off('initialized', this.__i18next_callback__)
+      i18next.store.off('languageChanged', this.__i18next_callback__)
+      i18next.store.off('added', this.__i18next_callback__)
     }
   }
